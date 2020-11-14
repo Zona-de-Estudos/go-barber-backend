1dcbf044071a9e62689ad4dbbbdd6394da23d571
John Vidal <ba.john.vidal@gmail.com>
2020-11-13 23:37:50 -0300

    creating appointments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
-import { Router } from "express";
+import { Router } from 'express';
+import AppointmentsRouter from './appointments.routes';
 
 const routes = Router();
+
+routes.use('/appointments', AppointmentsRouter);
 
 export default routes;
