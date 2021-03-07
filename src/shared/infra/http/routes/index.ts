4f942ac37b2cbcdfdf9066cf5bef2033def117ff
John Vidal <ba.john.vidal@gmail.com>
2021-03-07 19:40:37 -0300

    User APIs working

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import UsersRouter from '@modules/users/infra/http/routes/users.routes';
 import SessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import AppointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
 routes.use('/users', UsersRouter);
 routes.use('/sessions', SessionsRouter);
 routes.use('/appointments', AppointmentsRouter);
+routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
