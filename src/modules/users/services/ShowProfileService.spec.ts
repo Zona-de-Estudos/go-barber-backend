9527f9e1bc6b8375746031fc862dd1c95f4e077b
John Vidal <ba.john.vidal@gmail.com>
2021-03-07 23:04:26 -0300

    test 100%

--- src/modules/users/services/ShowProfileService.spec.ts
+++ src/modules/users/services/ShowProfileService.spec.ts
@@ -1,7 +1,7 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
-import ShowProfileService from './ShowProfileServices';
+import ShowProfileService from './ShowProfileService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let showProfile: ShowProfileService;
